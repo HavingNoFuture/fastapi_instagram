b8ad85963a6b033e0ab3c404483cc7567a27d57a
Alexey Ermilov <alex.erm@yandex.ru>
2022-04-30 16:22:11 +0300

    Users API: Added SignUp. Renamed app to auth

---
+++ app/auth/services.py
@@ -0,0 +1,48 @@
+from passlib.context import CryptContext
+
+from app.auth.models import User
+from app.auth.schemas import UserSignUpPostData
+from app.db import Session
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    """
+    Возвращает подтверждение пароля
+    :param plain_password: Пароль в НЕзахешированном виде
+    :param hashed_password: Пароль в захешированном виде
+    :return: Соответвтсвует ли пароль хешу
+    """
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password: str) -> str:
+    """
+    Возвращает хеш пароля
+    :param password: Пароль
+    :return: Хеш пароля
+    """
+    return pwd_context.hash(password)
+
+
+# region: User
+
+
+async def create_user(item: UserSignUpPostData) -> dict:
+    """
+    Создание пользователя в БД
+    :param item: Данные пользователя для создания: email и пароль
+    :return: Данные созданного пользователя: id и email
+    """
+    data = item.dict()
+    data['password'] = get_password_hash(data['password'])
+    with Session() as s:
+        user = User(**data)
+        s.add(user)
+        s.commit()
+        user_data = {'id': user.id, 'email': user.email}
+    return user_data
+
+
+# endregion: User
